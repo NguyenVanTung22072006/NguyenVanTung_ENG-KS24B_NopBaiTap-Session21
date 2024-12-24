839ad4e432a0451e1ee1cace9955e1273ff70334
NguyenVanTung22072006 <nvtung2207@gmail.com>
2024-12-24 21:46:23 +0700

    Bai 4

---
+++ Session21_Bai4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+    FILE *fptr;
+    char line[500];
+    fptr=fopen("bt01.txt", "r");
+    if(fptr==NULL){
+        printf("Khong the mo file\n");
+        return 1;
+    }
+    if(fgets(line, sizeof(line), fptr) != NULL){
+        printf("Dong dau tien trong file la: %s", line);
+    }else{
+        printf("File rong\n");
+    }
+    fclose(fptr);
+
+    return 0;
+}
